6609df3783352a44be5fdfd5b7ea5cc8a820582c
safran6ix <mohammedsafran6ix@gmail.com>
2025-05-25 00:28:45 +0530

    Modified

---
+++ script.js
@@ -0,0 +1,11 @@
+var password = document.getElementById("password");
+function genPassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passLenght = 15;
+    var password = "";
+    for (var i = 0; i < passLenght; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    document.getElementById("password").value = password;
+}
